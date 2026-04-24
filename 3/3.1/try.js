7f6bc129deffc735ed846b754d26736912d23457
55Pranjal <567pranjalagarwal@gmail.com>
2026-04-24 16:45:53 +0530

    Added 3.1

---
+++ 3/3.1/try.js
@@ -0,0 +1,30 @@
+let a = 5;
+const b = 10;
+
+// console.log(a);
+// console.log(b);
+
+// a = 15;
+
+// console.log(a);
+// console.log(b);
+
+// const string = "Hello";
+// const number = 3.14;
+// const arr = ["world", 31.4, [1, 2]];
+// const obj = { name: "Pranjal", age: 22 };
+
+// console.log(string);
+// console.log(number);
+// console.log(arr[2]);
+// console.log(obj.age);
+
+console.log(a + b);
+
+const c = 1; //Number
+const d = "1"; //String
+
+console.log(c == d);
+console.log(c === d);
+
+console.log(c >= d);
