0ef6e946294d233c120d8e651fd93d167e59fdfa
55Pranjal <567pranjalagarwal@gmail.com>
2026-04-25 20:37:42 +0530

    Added 3.2

---
+++ 3/3.2/try.js
@@ -0,0 +1,33 @@
+// const age = 20;
+
+// if (age >= 18) {
+//   console.log("adult");
+// } else {
+//   console.log("not adult");
+// }
+
+// let i;
+
+// for (i = 0; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// let c = 9;
+
+// while (c > 0) {
+//   console.log(c);
+//   c--;
+// }
+
+let day = "Monday";
+
+switch (day) {
+  case "Monday":
+    console.log("Today is Monday");
+  case "Tuesday":
+    console.log("Today is Tuesday");
+  case "Wednesday":
+    console.log("Today is Wednesday");
+  case "Thursday":
+    console.log("Today is Thursday");
+}
