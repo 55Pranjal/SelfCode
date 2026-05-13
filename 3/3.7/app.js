418524ed0db0d0f19e90bf55defc73bf5e4d07e5
55Pranjal <567pranjalagarwal@gmail.com>
2026-05-13 22:55:32 +0530

    3.7

--- 3/3.7/app.js
+++ 3/3.7/app.js
@@ -1,11 +1,14 @@
-const title = document.querySelector("#title");
-const button = document.querySelector("button");
+let title = document.querySelector("h1");
+let button = document.querySelector("button");
 
 button.addEventListener("click", () => {
   fetch("https://jsonplaceholder.typicode.com/posts/1")
     .then((res) => res.json())
-
     .then((data) => {
-      title.innerText = data.title;
-    });
+      console.log(data);
+    })
+    .then((data) => {
+      title.innerText = data.id;
+    })
+    .catch((err) => console.log(err));
 });
